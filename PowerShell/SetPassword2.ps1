faa7e2235e7e18eec55aa5a6f6fd2bf1a28a91df
TesLA_422 <tesla422zxc@outlook.com>
2017-06-13 17:22:46 +0800

    Create SetPassword2.ps1

---
+++ PowerShell/SetPassword2.ps1
@@ -0,0 +1,21 @@
+#set code password
+$Credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content $env:userprofile\password.txt
+
+$UserName = “root”
+$PassphraseFile=”$env:userprofile\password.txt”
+$ourEncryptPw =  Get-Content $PassphraseFile | ConvertTo-SecureString
+$ourCreds =  New-Object System.Management.Automation.PSCredential ($UserName, $ourEncryptPw)
+$ourDecryptPw = $ourCreds.GetNetworkCredential().Password
+echo $ourDecryptPw
+
+#set code password
+$Credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content $env:userprofile\password.txt
+ 
+$UserName = 'scxuser'
+$PassphraseFile= "$env:userprofile\password.txt"
+$ourEncryptPw =  Get-Content $PassphraseFile | ConvertTo-SecureString
+$ourCreds =  New-Object System.Management.Automation.PSCredential ($UserName, $ourEncryptPw)
+$ourDecryptPw = $ourCreds.GetNetworkCredential().Password
+echo $ourDecryptPw
