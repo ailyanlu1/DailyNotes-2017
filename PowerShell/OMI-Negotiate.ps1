9bb0447c864ef2db3285a75e1a72e57af46ca67d
TesLA_422 <tesla422zxc@outlook.com>
2017-06-13 17:21:41 +0800

    Create OMI-Negotiate.ps1

---
+++ PowerShell/OMI-Negotiate.ps1
@@ -0,0 +1,10 @@
+$PWord = convertto-securestring '05eOsG^17' -asplaintext -force
+$Cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList omi-cent7x64-t1\root, $PWord
+$CimSessionOption = New-CimSessionOption -UseSsl:$false -SkipCACheck:$true -SkipCNCheck:$true -SkipRevocationCheck:$true -NoEncryption
+$CimSession=New-CimSession -Credential:$Cred -ComputerName:omi-cent7x64-t1 -Port:5985 -Authentication:Negotiate -SessionOption:$CimSessionOption
+Register-CimIndicationEvent -ClassName 'XYZ_DiskFault' -CimSession $CimSession -SourceIdentifier 'DiskFaultTest:omi-cent7x64-t1'
+Start-Sleep -s 3
+(Get-Event -SourceIdentifier 'DiskFaultTest:omi-cent7x64-t1')[0].SourceEventArgs.NewEvent
+
+Remove-Event -SourceIdentifier 'DiskFaultTest:omi-cent7x64-t1'
+Unregister-Event -SourceIdentifier 'DiskFaultTest:omi-cent7x64-t1'
